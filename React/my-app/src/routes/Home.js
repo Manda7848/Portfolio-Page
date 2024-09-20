cfc87977013963663f6e1436f948e59e0fdf28b4
Manda7848 <manda7848code@gmail.com>
2024-09-20 05:56:54 +0100

    Analytics!?

--- React/my-app/src/routes/Home.js
+++ React/my-app/src/routes/Home.js
@@ -2,7 +2,9 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import HeroImg from "../Components/heroimg";
 import Footer from "../Components/footer";
-import Work from "../Components/work"
+import Work from "../Components/work";
+import Analytics  from "@vercel/analytics/react";
+// import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const Home = () => {
     return  <div>
@@ -11,8 +13,10 @@ const Home = () => {
     <h1 style={{ paddingTop: "10px", textAlign: "center" }}>Projects</h1>
     <Work/>
     <Footer />
-    </div>;
+    <Analytics/>
+        </div>;
     
+    // <SpeedInsights/>
 };
 
 export default Home;
